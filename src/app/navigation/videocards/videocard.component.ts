6b6dbf80c707d2c8a05ff35e1127c4ab60de5d2a
Dylan <dds00002@mail.wvu.edu>
2022-02-10 08:28:15 -0500

    Added 6 new components with reusable classes

--- src/app/navigation/videocards/videocard.component.ts
+++ src/app/navigation/videocards/videocard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-videocards',
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['videocards.component.css']
 })
 export class VideoCardsComponent {
+  @Input()
+  img: string;
+  viewcountlow: string; 
+  viewcountmid: string;
+  viewcounthigh: string; 
+  owner: string;
+  date: string;
+  title: string;
+
+  constructor() {
+    this.img = "";
+    this.viewcountlow = "34K";
+    this.viewcountmid = "368K";
+    this.viewcounthigh = "3.2M";
+    this.owner = "Science Daily";
+    this.date = "";
+    this.title = "";
+  }
+
 }
---
+++ src/app/video/mock_video_item.ts
@@ -0,0 +1,81 @@
+export const mock_video_list = [
+    {
+        img: "/assets/world-war-3.webp",
+        viewcountlow: "38K",
+        owner: "Science Daily",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+    },
+    {
+        img: "/assets/index.jpeg",
+        viewcountlow: "38K",
+        owner: "CNN",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/biden-michigan-ap-rc-200909_hpMain_16x9_1600.jpg",
+        viewcountlow: "38K",
+        owner: "Fox News",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/b2.jpeg",
+        viewcountlow: "38K",
+        owner: "Science Daily",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/andromeda.jpeg",
+        viewcountlow: "38K",
+        owner: "Science Daily",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/hummus.jpeg",
+        viewcountlow: "38K",
+        owner: "Gordon Ramsay",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/volcano.jpg",
+        viewcountlow: "38K",
+        owner: "Science Daily",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/batman.webp",
+        viewcountlow: "38K",
+        owner: "IMDB",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/indexlas.jpeg",
+        viewcountlow: "38K",
+        owner: "Gordon Ramsay",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+    {
+        img: "/assets/cave.webp",
+        viewcountlow: "38K",
+        owner: "Science Daily",
+        date: "3 weeks ago",
+        title: "WWIII: Final Frontier"
+
+    },
+]
