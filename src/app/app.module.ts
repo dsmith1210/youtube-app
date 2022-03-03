5692e3f39bf9f668a3472cb63007276eafa26807
Dylan <dds00002@mail.wvu.edu>
2022-03-02 21:29:15 -0500

    Implemented user info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
+import { UserInfoComponent } from './header/user-info.component';
 
 import { AppComponent } from './app.component';
 import { BigCardComponent } from './navigation/bigcard/bigcard.component';
@@ -16,11 +18,12 @@ import { Video1Component } from './video/card1-layout.component';
     BigCardComponent,
     SideNavBarComponent,
     VideoCardsComponent,
-    
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/user-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: "youtube-user-info",
+    templateUrl: "user-info.component.html"
+})
+
+export class UserInfoComponent {
+
+}
